84b1a6a720c3ecefdeeb710afc6c7602da2448db
Marc Minor <marceminor@gmail.com>
2013-11-08 18:02:55 -0500

    added new practice scripts from codecademy exploration.

--- codeac_hashes2.rb
+++ codeac_hashes2.rb
@@ -31,28 +31,65 @@ end
 
 =end
 
-prompt = "> "
 movies = {
     Braveheart: 4,
     Aliens: 3,
     Water_World: 1
 }
 
-puts "What would you like to do?"
-print prompt
+puts "What would you like to do? \nYou can add, update, display, or delete."
 choice = gets.chomp()
 
 choice.downcase!
 
 case choice
 when "add"
-    puts "Added!"
+    puts "What is the title of the movie you'd like to add?"
+    title = gets.chomp()
+    
+    until movies[title.to_sym].nil?
+      puts "Looks like that movie already exists! You can enter a different movie, update the rating or delete #{title}."
+      puts "What is the title of a new movie you'd like to add?"
+      title = gets.chomp()
+    end
+    
+    puts "What is the rating you'd like to give this movie (0-5)?"
+    rating = gets.chomp()
+    
+    until rating.to_i <= 5
+        puts "You entered \"#{rating}\". Your rating must be between 0-5. Try again."
+        puts "Please enter a rating between 0-5."
+        rating = gets.chomp()
+    end
+    
+    movies[title.to_sym] = rating.to_i
+    puts "Awesome! \"#{title}\" was added with a #{rating}-star rating!"
+
 when "update"
-    puts "Updated!"
+    puts "What movie do you want to update?"
+    title = gets.chomp()
+    
+    if movies[title.to_sym].nil?
+        puts "Error! There isn't a movie with that name in the system. Try again"
+    else
+        puts "What should the new rating be? #{title}'s current rating is #{movies[title.to_sym]}."
+        rating = gets.chomp()
+        movies[title.to_sym] = rating.to_i
+        puts "Awesome your new rating for #{title} is #{movies[title.to_sym]}!"
+    end
 when "display"
-    puts "Movies!"
+    movies.each do |movie, rating|
+        puts "#{movie}: #{rating}"
+end
 when "delete"
-    puts "Deleted!"
+    puts "What movie do you want to delete?"
+    title = gets.chomp
+    if movies[title.to_sym].nil?
+        puts "Sorry the movie \"#{title}\" isn't in the database."
+    else
+        movies.delete(title.to_sym)
+        puts "Sounds good! \"#{title}\" has been deleted"
+    end
 else
-    puts "Error!"
+    puts "Error! There is no command for \"#{choice}\". You can add, update, display, or delete."
 end
---
+++ codeac_practice.rb
@@ -0,0 +1,47 @@
+# Type your Ruby code below!
+brothers = ["matt", "trey", "marc"]
+
+puts "Type the first name of a Minor brother."
+name = gets.chomp()
+puts "You nailed it. \"#{name}\" is a Minor brother!" if brothers.include?(name) == true
+puts "Nope. \"#{name}\" is not a Minor brother." if brothers.include?(name) == false
+
+# Type your Ruby code below!
+puts "How many Minor brothers are there?"
+total_brothers = gets.chomp()
+puts "nope that's wrong. Try again." unless total_brothers.to_i == brothers.count
+puts "yup that's correct! There are #{total_brothers} Minor brothers." if total_brothers.to_i == brothers.count
+
+# Type your Ruby code below!
+parents = ["Rob", "Cindy"]
+
+puts "Type the first name of one of the Minor parents."
+parent_name = gets.chomp()
+puts parents.include?(parent_name) ? "Yup! \"#{parent_name}\" is a Minor parent." : "Nope! \"#{parent_name}\" is not a Minor parent."
+
+puts "Hello there!"
+greeting = gets.chomp
+
+# Add your case statement below!
+case greeting
+ when "English" then puts "Hello!"
+ when "French" then puts "Bonjour!"
+ when "German" then puts "Guten Tag!"
+ when "Finnish" then puts "Haloo!"
+ else puts "I don't know that language!"
+end
+
+
+def a
+  puts "A was evaluated!"
+  return true
+end
+
+def b
+  puts "B was also evaluated!"
+  return true
+end
+
+puts a || b
+puts "------"
+puts a && b
